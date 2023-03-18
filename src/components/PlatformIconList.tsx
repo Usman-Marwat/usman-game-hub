d5d0d19c9b47f24d359ee3a4ff2fa4777bb49121
Usman-Marwat <us895907a2@gmail.com>
2023-03-18 09:26:29 +0500

    Update page title to verify vercel changes

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -6,9 +6,9 @@ import {
   FaLinux,
   FaAndroid,
 } from "react-icons/fa";
-import { MdPhoneIphone } from 'react-icons/md';
-import { SiNintendo } from 'react-icons/si';
-import { BsGlobe } from 'react-icons/bs';
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
 import { HStack, Icon } from "@chakra-ui/react";
 import { Platform } from "../hooks/useGames";
 import { IconType } from "react-icons";
@@ -18,25 +18,35 @@ interface Props {
 }
 
 const PlatformIconList = ({ platforms }: Props) => {
-  const iconMap: { [key: string]: IconType } = { 
+  const iconMap: { [key: string]: IconType } = {
     pc: FaWindows,
     playstation: FaPlaystation,
     xbox: FaXbox,
     nintendo: SiNintendo,
     mac: FaApple,
-    linux: FaLinux, 
+    linux: FaLinux,
     android: FaAndroid,
     ios: MdPhoneIphone,
-    web: BsGlobe
-  }
+    web: BsGlobe,
+  };
 
   return (
-    <HStack marginY={1}> 
+    <HStack marginY={1}>
       {platforms.map((platform) => (
-        <Icon key={platform.id} as={iconMap[platform.slug]} color='gray.500'/>
+        <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
       ))}
     </HStack>
   );
 };
 
 export default PlatformIconList;
+
+/*
+            [key: string]
+This is called an index signature. So this represents a key or property in this object
+By using the "Index Signature" syntax we don't have to specify the name of these keys.
+We just say we have any number of keys of type string.
+Secondly, each key is mapped to a value of type IconType.
+
+
+*/
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "62f3d719ed334f489c4481bff764b0fb",
   },
 });
