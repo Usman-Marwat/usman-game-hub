87201614c5ed98d6adb80cd65780f082a3fa2c60
Usman-Marwat <us895907a2@gmail.com>
2023-05-03 14:22:05 +0500

    Refator: Fetch games with react query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,34 +1,51 @@
-import { GameQuery } from "../App";
-import useData from "./useData";
-import { Genre } from "./useGenres";
+import { useQuery } from '@tanstack/react-query';
+import { GameQuery } from '../App';
+import apiClient, { FetchResponse } from '../services/api-client';
 
 export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
+	id: number;
+	name: string;
+	slug: string;
 }
 
 export interface Game {
-  id: number;
-  name: string;
-  background_image: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-  rating_top: number;
+	id: number;
+	name: string;
+	background_image: string;
+	parent_platforms: { platform: Platform }[];
+	metacritic: number;
+	rating_top: number;
 }
 
 const useGames = (gameQuery: GameQuery) =>
+	useQuery<FetchResponse<Game>, Error>({
+		queryKey: ['games', gameQuery],
+		queryFn: () =>
+			apiClient
+				.get<FetchResponse<Game>>('/games', {
+					params: {
+						genres: gameQuery.genre?.id,
+						parent_platforms: gameQuery.platform?.id,
+						ordering: gameQuery.sortOrder,
+						search: gameQuery.searchText,
+					},
+				})
+				.then((res) => res.data),
+	});
+
+export default useGames;
+
+/*
+const useGames = (gameQuery: GameQuery) =>
   useData<Game>(
     "/games",
-    {
-      params: {
-        genres: gameQuery.genre?.id,
-        platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText,
-      },
-    },
+    .
+    .
+    .
     [gameQuery]
   );
 
-export default useGames;
+  We used the useData hook to fetch games. Now we replaced it with query hook from react Query
+  becasue all the functionality we have implemneted in the data hook is laready implementred in 
+  the query hook in a much better way.
+*/
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
+
+export interface FetchResponse<T> {
+	count: number;
+	results: T[];
+}
 
 export default axios.create({
-  baseURL: "https://api.rawg.io/api",
-  params: {
-    key: "62f3d719ed334f489c4481bff764b0fb",
-  },
+	baseURL: 'https://api.rawg.io/api',
+	params: {
+		key: '62f3d719ed334f489c4481bff764b0fb',
+	},
 });
