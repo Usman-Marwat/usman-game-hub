d1443865bcaf21775acc52b02f3de0ea25a6f3a7
Usman-Marwat <us895907a2@gmail.com>
2023-05-03 22:35:47 +0500

    Fetch game details

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,5 +1,6 @@
 //In this file we specify that all pages should have navigation bar at the top
 
+import { Box } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 
@@ -7,7 +8,9 @@ function Layout() {
 	return (
 		<>
 			<NavBar />
-			<Outlet />
+			<Box padding={5}>
+				<Outlet />
+			</Box>
 		</>
 	);
 }
