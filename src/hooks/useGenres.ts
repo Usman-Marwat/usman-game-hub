99a75cc84c012b90ebaebb95fcbdad1a9627079c
Usman-Marwat <us895907a2@gmail.com>
2023-05-04 11:46:47 +0500

    Move entity interfaces

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,14 +2,9 @@ import { useQuery } from '@tanstack/react-query';
 import ms from 'ms';
 import genres from '../data/genres';
 import APICLIENT from '../services/api-client';
+import { Genre } from '../entities/Genre';
 
 const apiClient = new APICLIENT<Genre>('/genres');
-
-export interface Genre {
-	id: number;
-	name: string;
-	image_background: string;
-}
 
 const useGenres = () =>
 	useQuery({
