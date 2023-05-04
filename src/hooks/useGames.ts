938cfc1621346d737d34ef255c741333788a5dc7
Usman-Marwat <us895907a2@gmail.com>
2023-05-04 15:38:49 +0500

    Refactor: entity interfaces

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,7 +2,7 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 import ms from 'ms';
 import APICLIENT, { FetchResponse } from '../services/api-client';
 import useGameQueryStore from '../store';
-import { Game } from '../entities/Game';
+import Game from '../entities/Game';
 
 const apiClient = new APICLIENT<Game>('/games');
 
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import ms from 'ms';
 import platforms from '../data/platforms';
 import APICLIENT from '../services/api-client';
-import { Platform } from '../entities/Platform';
+import Platform from '../entities/Platform';
 
 const apiClient = new APICLIENT<Platform>('platforms/lists/parents');
 
