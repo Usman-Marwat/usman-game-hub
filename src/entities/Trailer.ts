44711e5881c98411d2c279d5f3553e99041f73ad
Usman-Marwat <us895907a2@gmail.com>
2023-05-04 15:05:41 +0500

    Build game trailer

---
+++ src/entities/Trailer.ts
@@ -0,0 +1,11 @@
+export interface Trailer {
+	id: number;
+	name: string;
+	preview: string;
+	data: { 480: string; max: string };
+}
+/*
+"data:any"
+
+This is a temporary solution untill we find the shape of the object
+*/
---
+++ src/hooks/useTrailers.ts
@@ -0,0 +1,14 @@
+import { useQuery } from '@tanstack/react-query';
+import { Trailer } from '../entities/Trailer';
+import APICLIENT from '../services/api-client';
+
+const useTrailers = (gameId: number) => {
+	const apiClient = new APICLIENT<Trailer>(`/games/${gameId}/movies`);
+
+	return useQuery({
+		queryKey: ['trailers', gameId],
+		queryFn: apiClient.getAll,
+	});
+};
+
+export default useTrailers;
